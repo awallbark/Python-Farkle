cfc3070d1c35d467721f9aebfe296bb23afe9852
awallbark <39318690+awallbark@users.noreply.github.com>
2022-11-12 17:41:23 -0600

    Added logic to hold the score of 5 dice

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ import random
 
 score_dice = []
 
-roll_dice = random.randint(1, 7)
+def roll_dice():
+    die = random.randint(1, 5)
+    return die
 
+number_of_times_rolled = 0
 
+while number_of_times_rolled < 5:
+    roll = roll_dice()
+    score_dice.append(roll)
+    number_of_times_rolled += 1
+
+print(score_dice)
 
 
 
