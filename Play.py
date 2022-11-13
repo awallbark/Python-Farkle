fd693bef825fbfb84aa2e4f447764c431762dd5b
awallbark <39318690+awallbark@users.noreply.github.com>
2022-11-13 11:27:29 -0600

    Learning

---
+++ Play.py
@@ -0,0 +1,22 @@
+list = [1, 2, 1, 4, 1]
+current_score = 0
+
+for i in list:
+    print(list[i])
+
+
+
+
+# if 1 or 5 in list:
+#     for i in range(1, len(list)):
+#         if list.index(i) == 1:
+#             current_score += 100
+#     #print("You have a scoring roll!")
+#     #print(list)
+#         elif i == 5:
+#             current_score += 50
+# else:
+#     print(list)
+#     print("You Farkled! Better luck next time")
+#
+# print(current_score)
