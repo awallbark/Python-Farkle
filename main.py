24c0cbe2a9edbf54f9c27e47c1cf5879df9c9b82
awallbark <39318690+awallbark@users.noreply.github.com>
2022-11-13 11:11:20 -0600

    Cleaned up basic roll logic to get initial scoring correct

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import random
 
+
 initial_roll = []
-current_score = []
-roll = []
+current_score = 0
+game_rounds = []
 
 def roll_dice():
     die1 = random.randint(1, 5)
@@ -17,15 +18,18 @@ def roll_dice():
     initial_roll.append(die5)
 
 roll = roll_dice()
-if 1 in initial_roll:
+if 1 or 5 in initial_roll:
     print("You have a scoring roll!")
     print(initial_roll)
-elif 5 in initial_roll:
-    print("You have a scoring roll!")
-    print(initial_roll)
+    for i in initial_roll:
+        if i == 1:
+            current_score += 100
+        elif i == 5:
+            current_score += 50
 else:
     print(initial_roll)
     print("You Farkled! Better luck next time")
 
+print(current_score)
 
 
