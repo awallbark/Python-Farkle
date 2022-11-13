855b0bfc471982a99a18eab31618f5c16651bb28
awallbark <39318690+awallbark@users.noreply.github.com>
2022-11-13 11:43:56 -0600

    Playing with dictionaries

--- Play.py
+++ Play.py
@@ -1,11 +1,9 @@
-list = [1, 2, 1, 4, 1]
-current_score = 0
-
-for i in list:
-    print(list[i])
-
-
-
+import random
+# list = [1, 2, 1, 4, 1]
+# current_score = 0
+#
+# for i in list:
+#     print(list[i])
 
 # if 1 or 5 in list:
 #     for i in range(1, len(list)):
@@ -20,3 +18,23 @@ for i in list:
 #     print("You Farkled! Better luck next time")
 #
 # print(current_score)
+
+initial_roll = {}
+
+def roll_dice():
+    die1 = random.randint(1, 5)
+    initial_roll.update({"die1": die1})
+    die2 = random.randint(1, 5)
+    initial_roll.update({"die2": die2})
+    die3 = random.randint(1, 5)
+    initial_roll.update({"die3": die3})
+    die4 = random.randint(1, 5)
+    initial_roll.update({"die4": die4})
+    die5 = random.randint(1, 5)
+    initial_roll.update({"die5": die5})
+
+roll_dice()
+# print(initial_roll)
+
+if initial_roll.values() == 1 or initial_roll.values() == 5:
+    print(initial_roll.values())
