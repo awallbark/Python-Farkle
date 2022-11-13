a9e51c660a85391f0987ff9d78f6c6891a988eaa
awallbark <39318690+awallbark@users.noreply.github.com>
2022-11-12 19:23:26 -0600

    renamed inital_score_dice to intial_roll

--- main.py
+++ main.py
@@ -1,31 +1,28 @@
 import random
 
-initial_score_dice = []
+initial_roll = []
 current_score = []
 roll = []
 
 def roll_dice():
     die1 = random.randint(1, 5)
-    initial_score_dice.append(die1)
+    initial_roll.append(die1)
     die2 = random.randint(1, 5)
-    initial_score_dice.append(die2)
+    initial_roll.append(die2)
     die3 = random.randint(1, 5)
-    initial_score_dice.append(die3)
+    initial_roll.append(die3)
     die4 = random.randint(1, 5)
-    initial_score_dice.append(die4)
+    initial_roll.append(die4)
     die5 = random.randint(1, 5)
-    initial_score_dice.append(die5)
+    initial_roll.append(die5)
 
 roll = roll_dice()
-for i in initial_score_dice:
-    if initial_score_dice.index(i) == 1 or initial_score_dice.index(i) == 5:
-        print("You have a scoring roll!")
-        print(initial_score_dice)
-        break
-    else:
-        print(initial_score_dice)
-        print("You Farkled! Better luck next time")
-        break
+if initial_score_dice.index(1) or initial_score_dice.index(5):
+    print("You have a scoring roll!")
+    print(initial_score_dice)
+else:
+    print(initial_score_dice)
+    print("You Farkled! Better luck next time")
 
 
 
