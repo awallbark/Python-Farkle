c07db72d36f696a25c1e24fc5c8e0daae04a714b
awallbark <39318690+awallbark@users.noreply.github.com>
2022-11-13 21:22:21 -0600

    Removed for loop for socring logic

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
 
 
-initial_roll = [1, 1, 1, 1, 1, 1]
+initial_roll = []
 current_score = 0
 game_rounds = []
 ones_counter = 0
@@ -11,38 +11,37 @@ fours_counter = 0
 fives_counter = 0
 sixes_counter = 0
 
-# def roll_dice():
-#     die1 = random.randint(1, 6)
-#     initial_roll.append(die1)
-#     die2 = random.randint(1, 6)
-#     initial_roll.append(die2)
-#     die3 = random.randint(1, 6)
-#     initial_roll.append(die3)
-#     die4 = random.randint(1, 6)
-#     initial_roll.append(die4)
-#     die5 = random.randint(1, 6)
-#     initial_roll.append(die5)
+def roll_dice():
+    die1 = random.randint(1, 6)
+    initial_roll.append(die1)
+    die2 = random.randint(1, 6)
+    initial_roll.append(die2)
+    die3 = random.randint(1, 6)
+    initial_roll.append(die3)
+    die4 = random.randint(1, 6)
+    initial_roll.append(die4)
+    die5 = random.randint(1, 6)
+    initial_roll.append(die5)
 
 
-#roll = roll_dice()
-if 1 or 5 in initial_roll:
+roll = roll_dice()
+
+if initial_roll.count(1) or initial_roll.count(5):
     print("You have a scoring roll!")
     print(initial_roll)
-    for i in initial_roll:
-        if i == 1:
+    for a in initial_roll:
+        if a == 1:
             ones_counter += 1
-            #current_score += 100
-        elif i == 2:
+        elif a == 2:
             twos_counter += 1
-        elif i == 3:
+        elif a == 3:
             threes_counter += 1
-        elif i == 4:
+        elif a == 4:
             fours_counter += 1
-        elif i == 5:
+        elif a == 5:
             fives_counter += 1
-        elif i == 6:
+        elif a == 6:
             sixes_counter += 1
-            #current_score += 50
     if ones_counter == 1:
         current_score += 100
     elif ones_counter == 2:
@@ -52,12 +51,57 @@ if 1 or 5 in initial_roll:
     elif ones_counter == 4:
         current_score += 1000
     elif ones_counter == 5:
-        current_score == 2000
+        current_score += 2000
     elif ones_counter == 6:
         current_score += 3000
+    if twos_counter == 3:
+        current_score += 200
+    elif twos_counter == 4:
+        current_score += 1000
+    elif twos_counter == 5:
+        current_score += 2000
+    elif twos_counter == 6:
+        current_score += 3000
+    if threes_counter == 3:
+        current_score += 300
+    elif threes_counter == 4:
+        current_score += 1000
+    elif threes_counter == 5:
+        current_score += 2000
+    elif threes_counter == 6:
+        current_score += 3000
+    if fours_counter == 3:
+        current_score += 400
+    elif fours_counter == 4:
+        current_score += 1000
+    elif fours_counter == 5:
+        current_score += 2000
+    elif fours_counter == 6:
+        current_score += 3000
+    if fives_counter == 1:
+        current_score += 50
+    elif fives_counter == 2:
+        current_score += 100
+    elif fives_counter == 3:
+        current_score += 500
+    elif fives_counter == 4:
+        current_score += 1000
+    elif fives_counter == 5:
+        current_score += 2000
+    elif fives_counter == 6:
+        current_score += 3000
+    if sixes_counter == 3:
+        current_score += 600
+    elif sixes_counter == 4:
+        current_score += 1000
+    elif sixes_counter == 5:
+        current_score += 2000
+    elif sixes_counter == 6:
+        current_score += 3000
 else:
-    print(initial_roll)
-    print("You Farkled! Better luck next time")
+    if current_score == 0:
+        print("You Farkled! Better luck next time")
+        print(initial_roll)
 
 print(current_score)
 
