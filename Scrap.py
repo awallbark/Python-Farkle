ca2f7eb977148372def40f10f2ebc5242acb24d9
awallbark <39318690+awallbark@users.noreply.github.com>
2022-11-14 17:48:05 -0600

    moving scraps of code over for later use maybe

--- Scrap.py
+++ Scrap.py
@@ -1,7 +1,92 @@
-#this works for adding 1 & 5
-for i in initial_roll:
-    if i == 1:
-        current_score += 100
-    elif i == 5:
-        current_score += 50
-print(current_score)
+import random
+
+# #this works for adding 1 & 5
+# for i in initial_roll:
+#     if i == 1:
+#         current_score += 100
+#     elif i == 5:
+#         current_score += 50
+# print(current_score)
+
+#first way of writing the random die
+    # die1 = random.randint(1, 6)
+    # initial_roll.append(die1)
+    # die2 = random.randint(1, 6)
+    # initial_roll.append(die2)
+    # die3 = random.randint(1, 6)
+    # initial_roll.append(die3)
+    # die4 = random.randint(1, 6)
+    # initial_roll.append(die4)
+    # die5 = random.randint(1, 6)
+    # initial_roll.append(die5)
+
+# if ones_counter == 1:
+#     current_score += 100
+# elif ones_counter == 2:
+#     current_score += 200
+# elif ones_counter == 3:
+#     current_score += 300
+# elif ones_counter == 4:
+#     current_score += 1000
+# elif ones_counter == 5:
+#     current_score += 2000
+# elif ones_counter == 6:
+#     current_score += 3000
+# if twos_counter == 3:
+#     current_score += 200
+# elif twos_counter == 4:
+#     current_score += 1000
+# elif twos_counter == 5:
+#     current_score += 2000
+# elif twos_counter == 6:
+#     current_score += 3000
+# if threes_counter == 3:
+#     current_score += 300
+# elif threes_counter == 4:
+#     current_score += 1000
+# elif threes_counter == 5:
+#     current_score += 2000
+# elif threes_counter == 6:
+#     current_score += 3000
+# if fours_counter == 3:
+#     current_score += 400
+# elif fours_counter == 4:
+#     current_score += 1000
+# elif fours_counter == 5:
+#     current_score += 2000
+# elif fours_counter == 6:
+#     current_score += 3000
+# if fives_counter == 1:
+#     current_score += 50
+# elif fives_counter == 2:
+#     current_score += 100
+# elif fives_counter == 3:
+#     current_score += 500
+# elif fives_counter == 4:
+#     current_score += 1000
+# elif fives_counter == 5:
+#     current_score += 2000
+# elif fives_counter == 6:
+#     current_score += 3000
+# if sixes_counter == 3:
+#     current_score += 600
+# elif sixes_counter == 4:
+#     current_score += 1000
+# elif sixes_counter == 5:
+#     current_score += 2000
+# elif sixes_counter == 6:
+#     current_score += 3000
+
+# for i in dice_number:
+#     if dice_number.fromkeys("ones", 1):
+#         current_score += 100
+#     elif dice_number.fromkeys("ones", 2):
+#         current_score += 200
+#     elif dice_number.fromkeys("ones", 3):
+#         current_score += 300
+#     elif dice_number.fromkeys("ones", 4):
+#         current_score += 1000
+#     elif dice_number.fromkeys("ones", 5):
+#         current_score += 2000
+#     elif dice_number.fromkeys("ones", 6):
+#         current_score += 3000
