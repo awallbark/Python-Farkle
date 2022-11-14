8f0ecbe2980b8871ef27770441b1c28a4df629b9
awallbark <39318690+awallbark@users.noreply.github.com>
2022-11-13 21:53:54 -0600

    Updated logic for counting dice

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
 import random
 
 
-initial_roll = []
+roll = []
 current_score = 0
 #game_rounds = {"round": <value>}
-ones_counter = 0
-twos_counter = 0
-threes_counter = 0
-fours_counter = 0
-fives_counter = 0
-sixes_counter = 0
+dice_number = {}
 
 def roll_dice():
     die_count = 0
     while die_count <= 5:
-        die = random.randint(1,6)
-        initial_roll.append(die)
+        die = random.randint(1, 6)
+        roll.append(die)
         die_count += 1
 
 #first way of writing the random die
@@ -31,85 +26,100 @@ def roll_dice():
     # initial_roll.append(die5)
 
 
-roll = roll_dice()
+turn = roll_dice()
 
-if initial_roll.count(1) or initial_roll.count(5) or initial_roll.count(2) >= 3 or initial_roll.coun(3) >= 3 or initial_roll.count(4) >= 3 or initial_roll.count(6) >= 3:
-    print("You have a scoring roll!")
-    print(initial_roll)
-    for a in initial_roll:
-        if a == 1:
-            ones_counter += 1
-        elif a == 2:
-            twos_counter += 1
-        elif a == 3:
-            threes_counter += 1
-        elif a == 4:
-            fours_counter += 1
-        elif a == 5:
-            fives_counter += 1
-        elif a == 6:
-            sixes_counter += 1
-    if ones_counter == 1:
-        current_score += 100
-    elif ones_counter == 2:
-        current_score += 200
-    elif ones_counter == 3:
-        current_score += 300
-    elif ones_counter == 4:
-        current_score += 1000
-    elif ones_counter == 5:
-        current_score += 2000
-    elif ones_counter == 6:
-        current_score += 3000
-    if twos_counter == 3:
-        current_score += 200
-    elif twos_counter == 4:
-        current_score += 1000
-    elif twos_counter == 5:
-        current_score += 2000
-    elif twos_counter == 6:
-        current_score += 3000
-    if threes_counter == 3:
-        current_score += 300
-    elif threes_counter == 4:
-        current_score += 1000
-    elif threes_counter == 5:
-        current_score += 2000
-    elif threes_counter == 6:
-        current_score += 3000
-    if fours_counter == 3:
-        current_score += 400
-    elif fours_counter == 4:
-        current_score += 1000
-    elif fours_counter == 5:
-        current_score += 2000
-    elif fours_counter == 6:
-        current_score += 3000
-    if fives_counter == 1:
-        current_score += 50
-    elif fives_counter == 2:
-        current_score += 100
-    elif fives_counter == 3:
-        current_score += 500
-    elif fives_counter == 4:
-        current_score += 1000
-    elif fives_counter == 5:
-        current_score += 2000
-    elif fives_counter == 6:
-        current_score += 3000
-    if sixes_counter == 3:
-        current_score += 600
-    elif sixes_counter == 4:
-        current_score += 1000
-    elif sixes_counter == 5:
-        current_score += 2000
-    elif sixes_counter == 6:
-        current_score += 3000
-else:
-    if current_score == 0:
-        print("You Farkled! Better luck next time")
-        print(initial_roll)
+def count_dice(turn):
+    ones_counter = 0
+    twos_counter = 0
+    threes_counter = 0
+    fours_counter = 0
+    fives_counter = 0
+    sixes_counter = 0
+    if roll.count(1) or roll.count(5) or roll.count(2) >= 3 or roll.count(3) >= 3 or roll.count(4) >= 3 or roll.count(6) >= 3:
+        print("You have a scoring roll!")
+        print(roll)
+        for a in roll:
+            if a == 1:
+                ones_counter += 1
+                dice_number.update({"ones": ones_counter})
+            elif a == 2:
+                twos_counter += 1
+                dice_number.update({"twos": twos_counter})
+            elif a == 3:
+                threes_counter += 1
+                dice_number.update({"threes": threes_counter})
+            elif a == 4:
+                fours_counter += 1
+                dice_number.update({"fours": fours_counter})
+            elif a == 5:
+                fives_counter += 1
+                dice_number.update({"fives": fives_counter})
+            elif a == 6:
+                sixes_counter += 1
+                dice_number.update({"sixes": sixes_counter})
+        print(dice_number)
+            # if ones_counter == 1:
+            #     current_score += 100
+            # elif ones_counter == 2:
+            #     current_score += 200
+            # elif ones_counter == 3:
+            #     current_score += 300
+            # elif ones_counter == 4:
+            #     current_score += 1000
+            # elif ones_counter == 5:
+            #     current_score += 2000
+            # elif ones_counter == 6:
+            #     current_score += 3000
+            # if twos_counter == 3:
+            #     current_score += 200
+            # elif twos_counter == 4:
+            #     current_score += 1000
+            # elif twos_counter == 5:
+            #     current_score += 2000
+            # elif twos_counter == 6:
+            #     current_score += 3000
+            # if threes_counter == 3:
+            #     current_score += 300
+            # elif threes_counter == 4:
+            #     current_score += 1000
+            # elif threes_counter == 5:
+            #     current_score += 2000
+            # elif threes_counter == 6:
+            #     current_score += 3000
+            # if fours_counter == 3:
+            #     current_score += 400
+            # elif fours_counter == 4:
+            #     current_score += 1000
+            # elif fours_counter == 5:
+            #     current_score += 2000
+            # elif fours_counter == 6:
+            #     current_score += 3000
+            # if fives_counter == 1:
+            #     current_score += 50
+            # elif fives_counter == 2:
+            #     current_score += 100
+            # elif fives_counter == 3:
+            #     current_score += 500
+            # elif fives_counter == 4:
+            #     current_score += 1000
+            # elif fives_counter == 5:
+            #     current_score += 2000
+            # elif fives_counter == 6:
+            #     current_score += 3000
+            # if sixes_counter == 3:
+            #     current_score += 600
+            # elif sixes_counter == 4:
+            #     current_score += 1000
+            # elif sixes_counter == 5:
+            #     current_score += 2000
+            # elif sixes_counter == 6:
+            #     current_score += 3000
+    else:
+        if current_score == 0:
+            print("You Farkled! Better luck next time")
+            print(roll)
 
+count_dice(turn)
 print(current_score)
 
 
