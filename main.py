3db152d101456d502699051a887c585b7fe0c71b
awallbark <39318690+awallbark@users.noreply.github.com>
2022-11-14 17:47:52 -0600

    Working on Score dice logic

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import random
 
 
 roll = []
-current_score = 0
+
 #game_rounds = {"round": <value>}
-dice_number = {}
+
 
 def roll_dice():
     die_count = 0
@@ -13,22 +13,11 @@ def roll_dice():
         roll.append(die)
         die_count += 1
 
-#first way of writing the random die
-    # die1 = random.randint(1, 6)
-    # initial_roll.append(die1)
-    # die2 = random.randint(1, 6)
-    # initial_roll.append(die2)
-    # die3 = random.randint(1, 6)
-    # initial_roll.append(die3)
-    # die4 = random.randint(1, 6)
-    # initial_roll.append(die4)
-    # die5 = random.randint(1, 6)
-    # initial_roll.append(die5)
-
 
 turn = roll_dice()
 
 def count_dice(turn):
+    dice_number = {}
     ones_counter = 0
     twos_counter = 0
     threes_counter = 0
@@ -58,68 +47,71 @@ def count_dice(turn):
                 sixes_counter += 1
                 dice_number.update({"sixes": sixes_counter})
         print(dice_number)
-            # if ones_counter == 1:
-            #     current_score += 100
-            # elif ones_counter == 2:
-            #     current_score += 200
-            # elif ones_counter == 3:
-            #     current_score += 300
-            # elif ones_counter == 4:
-            #     current_score += 1000
-            # elif ones_counter == 5:
-            #     current_score += 2000
-            # elif ones_counter == 6:
-            #     current_score += 3000
-            # if twos_counter == 3:
-            #     current_score += 200
-            # elif twos_counter == 4:
-            #     current_score += 1000
-            # elif twos_counter == 5:
-            #     current_score += 2000
-            # elif twos_counter == 6:
-            #     current_score += 3000
-            # if threes_counter == 3:
-            #     current_score += 300
-            # elif threes_counter == 4:
-            #     current_score += 1000
-            # elif threes_counter == 5:
-            #     current_score += 2000
-            # elif threes_counter == 6:
-            #     current_score += 3000
-            # if fours_counter == 3:
-            #     current_score += 400
-            # elif fours_counter == 4:
-            #     current_score += 1000
-            # elif fours_counter == 5:
-            #     current_score += 2000
-            # elif fours_counter == 6:
-            #     current_score += 3000
-            # if fives_counter == 1:
-            #     current_score += 50
-            # elif fives_counter == 2:
-            #     current_score += 100
-            # elif fives_counter == 3:
-            #     current_score += 500
-            # elif fives_counter == 4:
-            #     current_score += 1000
-            # elif fives_counter == 5:
-            #     current_score += 2000
-            # elif fives_counter == 6:
-            #     current_score += 3000
-            # if sixes_counter == 3:
-            #     current_score += 600
-            # elif sixes_counter == 4:
-            #     current_score += 1000
-            # elif sixes_counter == 5:
-            #     current_score += 2000
-            # elif sixes_counter == 6:
-            #     current_score += 3000
-    else:
-        if current_score == 0:
-            print("You Farkled! Better luck next time")
-            print(roll)
+        return dice_number
 
-count_dice(turn)
-print(current_score)
+
+def score_dice(dice_number):
+    current_score = 0
+
+    for k, v in dice_number.items():
+        if k == "ones" and v == 1 and k == "twos" and v == 1 and k == "threes" and v == 1 and k == "fours" and v == 1 and k == "fives" and v == 1 and k == "sixes" and v == 1:
+            current_score += 1500
+        if k == "ones" and v == 6 or k == "twos" and v == 6 or k == "threes" and v == 6 or k == "fours" and v == 6 or k == "fives" and v == 6 or k == "sixes" and v == 6:
+            current_score += 3000
+        if k == "ones" and v == 5 or k == "twos" and v == 5 or k == "threes" and v == 5 or k == "fours" and v == 5 or k == "fives" and v == 5 or k == "sixes" and v == 5:
+            current_score += 2000
+        if k == "ones" and v == 4 or k == "twos" and v == 4 or k == "threes" and v == 4 or k == "fours" and v == 4 or k == "fives" and v == 4 or k == "sixes" and v == 4:
+            current_score += 1000
+        if k == "ones" and v == 1:
+            current_score += 100
+        elif k == "ones" and v == 2:
+            current_score += 200
+        elif k == "ones" and v == 3:
+            current_score += 300
+        elif k == "ones" and v == 4:
+            current_score += 1000
+        elif k == "ones" and v == 5:
+            current_score += 1500
+        if k == "twos" and v == 3:
+            current_score += 200
+        elif k == "twos" and v == 4:
+            current_score += 1000
+        elif k == "twos" and v == 5:
+            current_score += 1500
+        if k == "threes" and v == 3:
+            current_score += 300
+        elif k == "threes" and v == 4:
+            current_score += 1000
+        elif k == "threes" and v == 5:
+            current_score += 1500
+        if k == "fours" and v == 3:
+            current_score += 400
+        elif k == "fours" and v == 4:
+            current_score += 1000
+        elif k == "fours" and v == 5:
+            current_score += 1500
+        if k == "fives" and v == 1:
+            current_score += 50
+        elif k == "fives" and v == 2:
+            current_score += 100
+        elif k == "fives" and v == 3:
+            current_score += 500
+        elif k == "fives" and v == 4:
+            current_score += 1000
+        elif k == "fives" and v == 5:
+            current_score += 1500
+    print(current_score)
+    return current_score
+
+
+
+    # else:
+    #     if current_score == 0:
+    #         print("You Farkled! Better luck next time")
+    #         print(roll)
+
+count_dice = count_dice(turn)
+score_dice(count_dice)
+
 
 
