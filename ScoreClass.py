11e26144e767f5cbd52d865745a65d98c56eea4d
awallbark <39318690+awallbark@users.noreply.github.com>
2022-11-18 21:43:31 -0600

    Fixed score_dice method syntax

--- ScoreClass.py
+++ ScoreClass.py
@@ -2,7 +2,7 @@ import random
 
 
 class Score():
-
+    # game_rounds = {"round": <value>}
 
     # def __init__(self):
     #      score = self.score
@@ -18,7 +18,7 @@ class Score():
         return roll
 
 
-    def count_dice(roll):
+    def count_dice(self, roll):
         dice_number = {}
         ones_counter = 0
         twos_counter = 0
@@ -49,5 +49,63 @@ class Score():
                 elif a == 6:
                     sixes_counter += 1
                     dice_number.update({"sixes": sixes_counter})
-            print(dice_number)
-            return dice_number
+        else:
+                print("You Farkled! Better luck next time")
+                print(roll)
+                print(dice_number)
+        return dice_number
+
+    def score_dice(self, arg):
+        current_score = 0
+
+        for k, v in arg.items():
+            if k == "ones" and v == 1 and k == "twos" and v == 1 and k == "threes" and v == 1 and k == "fours" and v == 1 and k == "fives" and v == 1 and k == "sixes" and v == 1:
+                current_score += 1500
+            if k == "ones" and v == 6 or k == "twos" and v == 6 or k == "threes" and v == 6 or k == "fours" and v == 6 or k == "fives" and v == 6 or k == "sixes" and v == 6:
+                current_score += 3000
+            if k == "ones" and v == 5 or k == "twos" and v == 5 or k == "threes" and v == 5 or k == "fours" and v == 5 or k == "fives" and v == 5 or k == "sixes" and v == 5:
+                current_score += 2000
+            if k == "ones" and v == 4 or k == "twos" and v == 4 or k == "threes" and v == 4 or k == "fours" and v == 4 or k == "fives" and v == 4 or k == "sixes" and v == 4:
+                current_score += 1000
+            if k == "ones" and v == 3 and k == "twos" and v == 3:
+                current_score += 2500
+            if k == "ones" and v == 1:
+                current_score += 100
+            elif k == "ones" and v == 2:
+                current_score += 200
+            elif k == "ones" and v == 3:
+                current_score += 300
+            elif k == "ones" and v == 4:
+                current_score += 1000
+            elif k == "ones" and v == 5:
+                current_score += 1500
+            if k == "twos" and v == 3:
+                current_score += 200
+            elif k == "twos" and v == 4:
+                current_score += 1000
+            elif k == "twos" and v == 5:
+                current_score += 1500
+            if k == "threes" and v == 3:
+                current_score += 300
+            elif k == "threes" and v == 4:
+                current_score += 1000
+            elif k == "threes" and v == 5:
+                current_score += 1500
+            if k == "fours" and v == 3:
+                current_score += 400
+            elif k == "fours" and v == 4:
+                current_score += 1000
+            elif k == "fours" and v == 5:
+                current_score += 1500
+            if k == "fives" and v == 1:
+                current_score += 50
+            elif k == "fives" and v == 2:
+                current_score += 100
+            elif k == "fives" and v == 3:
+                current_score += 500
+            elif k == "fives" and v == 4:
+                current_score += 1000
+            elif k == "fives" and v == 5:
+                current_score += 1500
+        print(current_score)
+        return current_score
