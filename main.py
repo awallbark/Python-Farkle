247f855df924f722db40d76da4fb142a6e063ecb
awallbark <39318690+awallbark@users.noreply.github.com>
2022-11-18 22:55:22 -0600

    Started to play around with game class

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+import Game
 import ScoreClass
 
-#game_rounds = {"round": <value>}
+
+new_game = Game.Game
+new_game.start_game()
 
 
-turn = ScoreClass.Score()
-roll = turn.roll_dice()
-count = turn.count_dice(roll)
-scoring = turn.score_dice(count)
+
+# turn = ScoreClass.Score()
+# roll = turn.roll_dice()
+# count = turn.count_dice(roll)
+# scoring = turn.score_dice(count)
 # print(roll)
 # print(count)
 # print(scoring)
